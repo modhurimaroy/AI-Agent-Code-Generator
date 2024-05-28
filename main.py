325eb7b98d7109f04154c5147d992b8f29b6265c
Modhurima Roy Kenopy <modhurima@modhurimas-air.home>
2024-05-27 23:02:51 -0400

    Signed up and used LLAMA Parse

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 llm = Ollama( model = "mistral", request_timeout = 50.0)
 
-result = llm.complete("Hello world")
+parser = LlamaParse(result_type="markdown")
+
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+
+embed_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+query_engine = vector_index.as_query_engine(llm=llm)
+
+result = query_engine.query("what are some of the routes in the api?")
 print(result)
