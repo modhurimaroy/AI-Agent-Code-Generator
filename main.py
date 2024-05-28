b53f3edb5e194933f59e3842da7bf003dd9bc02a
Modhurima Roy Kenopy <modhurima@modhurimas-air.home>
2024-05-28 00:00:29 -0400

    Finished building the agent

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemp
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
-from prompts import context
+from pydantic import BaseModel
+from llama_index.core.output_parsers import PydanticOutputParser
+from llama_index.core.query_pipeline import QueryPipeline
+from prompts import context, code_parser_template
 from code_reader import code_reader
 from dotenv import load_dotenv
-
+import os
+import ast
 
 load_dotenv()
 
@@ -36,6 +40,43 @@ tools = [
 code_llm = Ollama(model="codellama")
 agent = ReActAgent.from_tools(tools, llm=code_llm, verbose=True, context=context)
 
-while(prompt := input("Enter a prompt(q to quit):")) != "q":
-    result = agent.query(prompt)
-    print(result)
+class CodeOutput(BaseModel):
+    code: str
+    description: str
+    filename: str
+
+parser = PydanticOutputParser(CodeOutput)
+json_prompt_str = parser.format(code_parser_template)
+json_prompt_tmpl = PromptTemplate(json_prompt_str)
+output_pipeline = QueryPipeline(chain=[json_prompt_tmpl, llm])
+
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    retries = 0
+
+    while retries < 3:
+        try:
+            result = agent.query(prompt)
+            next_result = output_pipeline.run(response=result)
+            cleaned_json = ast.literal_eval(str(next_result).replace("assistant:", ""))
+            break
+        except Exception as e:
+            retries += 1
+            print(f"Error occured, retry #{retries}:", e)
+
+    if retries >= 3:
+        print("Unable to process request, try again...")
+        continue
+
+    print("Code generated")
+    print(cleaned_json["code"])
+    print("\n\nDesciption:", cleaned_json["description"])
+
+    filename = cleaned_json["filename"]
+
+    try:
+        with open(os.path.join("output", filename), "w") as f:
+            f.write(cleaned_json["code"])
+        print("Saved file", filename)
+    except:
+        print("Error saving file...")
