ab24ec8b23391108a3f191534d68cfdaa98ef25c
Modhurima Roy Kenopy <modhurima@modhurimas-air.home>
2024-05-27 23:34:52 -0400

    Implemented a code reader

---
+++ prompts.py
@@ -0,0 +1,2 @@
+context = """Purpose: The primary role of this agent is to assist users by analyzing code. It should
+            be able to generate code and answer questions about code provided. """
